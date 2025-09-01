aa30da6e22a7695b9b0fe6fef81df4a0b48060c6
Miguel <migalindor@unal.edu.co>
2025-09-01 12:33:35 -0500

    first commit

---
+++ EjerciciosIF.py
@@ -0,0 +1,23 @@
+#Miguel Galindo
+
+
+#Operaciones logicas
+
+A= float(input ("Ingresa tu primer numero :"))
+B= float(input ("Ingrese tu segundo numero :"))
+print("En esta linea, se trabajara el comparativo <")
+if(A<B):
+    print (f"El primer numero {A} es menor que el segundo numero {B}")
+else:
+    print (f"El primer numero {A} no es mayor que el segundo numero {B}")
+print("En esta linea, se trabajara el comparativo <")
+if(A>B):
+    print (f"El primer numero {A} es mayor que el segundo numero {B}")
+else:
+    print (f"El primer numero {A}  es menor que el segundo numero {B}")
+print("En esta linea, se trabajara el comparativo =")
+if(A==B):
+    print ("Ambos numeros son iguales")
+else:
+    print (f"El primer numero {A} es diferente del segundo numero {B}")
+    515
