6e61ae5cdc17bd8d11b6b48915af18e73e4ffc13
Miguel <migalindor@unal.edu.co>
2025-09-01 12:57:09 -0500

    Agregue por ultimo el condicional diferente o igual

--- EjerciciosIF.py
+++ EjerciciosIF.py
@@ -20,7 +20,7 @@ if(A==B):
     print ("Ambos numeros si son iguales")
 else:
     print ("Los numeros no son iguales")
-print("En esta linea, se trabajara el comparativo mayor o igual")
+print("En esta linea, se trabajara el comparativo menor o igual")
 if(A<=B):
     if (A<B):
         print(f"El primer numero {A} si es menor que el segundo numero {B}")
@@ -36,3 +36,8 @@ if(A>=B):
         print(f"El primer numero {A} es igual que el segundo numero {B}")
 else:
     print (f"El primer numero {A} no es mayor o igual que el segundo numero {B}")
+    print("En esta linea, se trabajara el comparativo diferente")
+if(A!=B):
+    print("Los numeros si son diferes")
+else:
+    print("Los dos numeros son iguales")
