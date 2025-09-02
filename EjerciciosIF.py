24e63db9103b65ad14240d12d5265748720a103d
Miguel Galindo <migalindor@unal.edu.co>
2025-09-01 23:00:39 -0500

    revisando vi que podia mejorar los prints

--- EjerciciosIF.py
+++ EjerciciosIF.py
@@ -7,12 +7,12 @@ A= float(input ("Ingresa tu primer numero :"))
 B= float(input ("Ingrese tu segundo numero :"))
 print("En esta linea, se trabajara el comparativo <")
 if(A<B):
-    print (f"El primer numero {A} si es menor que el segundo numero {B}")
+    print (f"El numero {A} si es menor que el numero {B}")
 else:
     print ("No se cumple este condicional entre el primer y segundo numero")
 print("En esta linea, se trabajara el comparativo <")
 if(A>B):
-    print (f"El primer numero {A} si es mayor que el segundo numero {B}")
+    print (f"El  numero {A} si es mayor que el numero {B}")
 else:
     print ("No se cumple este condicional entre el primer y segundo numero")
 print("En esta linea, se trabajara el comparativo =")
@@ -23,21 +23,21 @@ else:
 print("En esta linea, se trabajara el comparativo menor o igual")
 if(A<=B):
     if (A<B):
-        print(f"El primer numero {A} si es menor que el segundo numero {B}")
+        print(f"El numero {A} si es menor que el numero {B}")
     if (A==B):
-        print(f"El primer numero {A} es igual que el segundo numero {B}")
+        print(f"El numero {A} es igual que el numero {B}")
 else:
-    print (f"El primer numero {A} no es menor o igual que el segundo numero {B}")
+    print (f"El numero {A} no es menor o igual que el numero {B}")
     print("En esta linea, se trabajara el comparativo mayor o igual")
 if(A>=B):
     if (A>B):
-        print(f"El primer numero {A} si es mayor que el segundo numero {B}")
+        print(f"El numero {A} si es mayor que el numero {B}")
     if (A==B):
-        print(f"El primer numero {A} es igual que el segundo numero {B}")
+        print(f"El numero {A} es igual que el numero {B}")
 else:
-    print (f"El primer numero {A} no es mayor o igual que el segundo numero {B}")
+    print (f"El numero {A} no es mayor o igual que el numero {B}")
     print("En esta linea, se trabajara el comparativo diferente")
 if(A!=B):
-    print("Los numeros si son diferes")
+    print("Los numeros si son diferentes")
 else:
     print("Los dos numeros son iguales")
--- Readme.md
+++ Readme.md
@@ -1,4 +1,5 @@
 # Condicionales
 ## Miguel Galindo
 
-### Aqui vamos a probar cada distinta combinacion 
+### Aqui vamos a probar cada distinta combinacion
+### Primero con el primer numero mayor que el segundo, 
